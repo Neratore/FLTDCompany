ca89daa03c91ab4368260ef42f63dd8a1833c837
Neratore <95870654+Neratore@users.noreply.github.com>
2022-06-03 20:45:45 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello. We are FLTD Company</h1>
+      <h2>And here wil apare our apps</h2>
+      <h3>fltd.ro,chat.fltd.ro/neratore.iaff.ro and more</h3>
+    </div>
+  );
+}
